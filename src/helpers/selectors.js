1dc4a7662538002ce915faac18b392fdcd6cf242
MackyClap <martimac2320@gmail.com>
2023-01-11 20:42:46 -0500

    install react hooks testing library

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -21,3 +21,23 @@ export function getAppointmentsForDay(state, day) {
   return result;
  
 }
+
+export function getInterview(state, interview) {
+  
+  let result = {};
+
+  if (interview === null) {
+    return null;
+  };
+
+  const arrayOfInterviewers =  Object.values(state.interviewers);
+
+  arrayOfInterviewers.forEach((interviewer) => {
+    if (interviewer.id === interview.interviewer) {
+      result = {student: interview.student, interviewer}
+    };
+  })
+
+  return result;
+}
+
