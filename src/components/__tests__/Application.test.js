2aa4555120a428fe7a1c5760056be0b60d06088c
MackyClap <martimac2320@gmail.com>
2023-01-27 21:25:36 -0500

    move visual mode tests to seperate test file

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -4,10 +4,6 @@ import { render, cleanup, waitForElement, fireEvent } from "@testing-library/rea
 
 import Application from "components/Application";
 
-import { renderHook, act } from "@testing-library/react-hooks";
-
-import useVisualMode from "hooks/useVisualMode";
-
 afterEach(cleanup);
 
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
@@ -20,58 +16,3 @@ it("defaults to Monday and changes the schedule when a new day is selected", () 
     });
 });
 
-const FIRST = "FIRST";
-
-const SECOND = "SECOND";
-
-const THIRD = "THIRD";
-
-test("useVisualMode should initialize with default value", () => {
-  const { result } = renderHook(() => useVisualMode(FIRST));
-
-  expect(result.current.mode).toBe(FIRST);
-});
-
-test("useVisualMode should transition to another mode", () => {
-  const { result } = renderHook(() => useVisualMode(FIRST));
-
-  act(() => {result.current.transition(SECOND)});
-  expect(result.current.mode).toBe(SECOND);
-});
-
-test("useVisualMode should return to previous mode", () => {
-  const { result } = renderHook(() => useVisualMode(FIRST));
-
-  act(() => {result.current.transition(SECOND)});
-  expect(result.current.mode).toBe(SECOND);
-
-  act(() => {result.current.transition(THIRD)});
-  expect(result.current.mode).toBe(THIRD);
-
-  act(() => {result.current.back()});
-  expect(result.current.mode).toBe(SECOND);
-
-  act(() => {result.current.back()});
-  expect(result.current.mode).toBe(FIRST);
-});
-
-test("useVisualMode should not return to previous mode if already at initial", () => {
-  const { result } = renderHook(() => useVisualMode(FIRST));
-
-  act(() => {result.current.back()});
-  expect(result.current.mode).toBe(FIRST);
-});
-
-test("useVisualMode should replace the current mode", () => {
-  const { result } = renderHook(() => useVisualMode(FIRST));
-
-  act(() => {result.current.transition(SECOND)});
-  expect(result.current.mode).toBe(SECOND);
-
-  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
-  act(() => {result.current.transition(THIRD, true)});
-  expect(result.current.mode).toBe(THIRD);
-
-  act(() => {result.current.back()});
-  expect(result.current.mode).toBe(FIRST);
-});
