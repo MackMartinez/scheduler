a2337894c3260a0f13c814e37a047b20f6652671
MackyClap <martimac2320@gmail.com>
2023-01-28 20:29:00 -0500

    test passes for should book an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,16 +1,27 @@
-
-describe("Navigation", () => {
-
+describe("interaction with appointment component", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+  
+    cy.visit("/");
+  
+    cy.contains("Monday");
+   });
+   
   it("Should book an interview", () => {
-      
-  });
-  
-  it("Should edit an interview", () => {
-  
-  });
-  
-  it("Should cancel an interview", () => {
+
+    cy.get("[alt=Add]").first().click();
+
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains("Save").click();
     
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
+  it("Should edit an interview", () => {});
+
+  it("Should cancel an interview", () => {});
 });
