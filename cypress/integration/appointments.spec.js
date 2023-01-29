1dcc9fa5e9ca5c765e56fabe2544ef6e6b0f4a14
MackyClap <martimac2320@gmail.com>
2023-01-28 20:09:51 -0500

    initial navigation cypress test

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,16 @@
+
+describe("Navigation", () => {
+
+  it("Should book an interview", () => {
+      
+  });
+  
+  it("Should edit an interview", () => {
+  
+  });
+  
+  it("Should cancel an interview", () => {
+    
+  });
+
+});
